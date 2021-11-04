b682d9b892f74357f76560407723f16da1344d7d
Hưng Thịnh <33023931+HungThinh0710@users.noreply.github.com>
2021-11-04 13:22:56 +0700

    Delete vars directory

--- vars/run/certtxgen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd $FABRIC_CFG_PATH
-# cryptogen generate --config crypto-config.yaml --output keyfiles
-configtxgen -profile OrdererGenesis -outputBlock genesis.block -channelID systemchannel
-
-configtxgen -printOrg ba-vku-udn-vn > JoinRequest_ba-vku-udn-vn.json
-configtxgen -printOrg it-vku-udn-vn > JoinRequest_it-vku-udn-vn.json
